9c99d18793966768794a3ea36cf2c66a0ab18c22
GorshkovTimur <gorshkoff.timur@gmail.com>
2019-10-05 15:41:05 +0300

    Have some question with part 2 of Lesson's HW

---
+++ app/src/main/java/com/timmyg/librariescourse/SecondView.java
@@ -0,0 +1,13 @@
+package com.timmyg.librariescourse;
+
+import moxy.MvpView;
+import moxy.viewstate.strategy.AddToEndSingleStrategy;
+import moxy.viewstate.strategy.StateStrategyType;
+
+public interface SecondView extends MvpView {
+
+    @StateStrategyType(value = AddToEndSingleStrategy.class)
+    void setTextViewResult(String valueForConcat);
+    String getEditText();
+
+}

25816296bba293faf07e24643bc99d11cd9478c5
GorshkovTimur <gorshkoff.timur@gmail.com>
2019-10-05 13:54:51 +0300

    1st part of HW done

--- app/src/main/java/com/timmyg/librariescourse/MainActivity.java
+++ app/src/main/java/com/timmyg/librariescourse/MainActivity.java
@@ -1,13 +1,51 @@
 package com.timmyg.librariescourse;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.timmyg.librariescourse.Presenter.MainPresenter;
+
+public class MainActivity extends AppCompatActivity implements MainView {
+
+    private static final String EMPTY_TEXT = "";
+    private EditText editText;
+    private TextView textView;
+    private Button button;
+
+    private MainPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        presenter = new MainPresenter(this);
+    }
+
+    private void initViews() {
+        editText = findViewById(R.id.edittext_for_concat);
+        button = findViewById(R.id.button_concat);
+        textView = findViewById(R.id.textview_for_result);
+    }
+
+    @Override
+    public void setTextViewResult(String value) {
+        textView.setText(value);
+
+    }
+
+    public void buttonClick(View view) {
+        presenter.onButtonClick();
+    }
+
+    public String getEditText(){
+        String text = editText.getText().toString();
+        editText.setText(EMPTY_TEXT);
+        return text;
     }
 }
---
+++ app/src/main/java/com/timmyg/librariescourse/MainView.java
@@ -0,0 +1,6 @@
+package com.timmyg.librariescourse;
+
+public interface MainView {
+    void setTextViewResult(String valueForConcat);
+    String getEditText();
+}
